0d7171b0c8ca955bd91234013a023450655ef388
Makoto Tanabe <xekid78@outlook.com>
2018-03-18 15:30:06 +0900

    Add files via upload

--- switch.js
+++ switch.js
@@ -1,21 +1,26 @@
-var omikuji = Math.floor(Math.random () * 6) + 1;
-switch (omikuji) {
-    case 1:
-        console.log("大吉");
-        break;
-    case 2:
-        console.log("中吉");
-        break;
-    case 3:
-        console.log("小吉");
-        break;
-    case 4:
-        console.log("吉");
-        break;
-    case 5:
-        console.log("凶");
-        break;
-    default:
-        console.log("大凶");
-        break;
-}
+(function () {
+    'use strict';
+
+    var omikuji = Math.floor(Math.random () * 6) + 1;
+    switch (omikuji) {
+        case 1:
+            console.log("大吉");
+            break;
+        case 2:
+            console.log("中吉");
+            break;
+        case 3:
+            console.log("小吉");
+            break;
+        case 4:
+            console.log("吉");
+            break;
+        case 5:
+            console.log("凶");
+            break;
+        default:
+            console.log("大凶");
+            break;
+    }
+
+})();
